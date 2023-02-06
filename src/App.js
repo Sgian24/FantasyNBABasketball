f9f686b3f6425d89c1d43c070457c6a021c79baf
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2023-02-06 15:02:19 -0500

    Filter out inactive players

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 const App = () => {
   const [activePlayers, setActivePlayers] = useState("");
-  const [players, setPlayers] = useState(false);
 
   const options = {
     method: 'GET',
@@ -12,6 +11,7 @@ const App = () => {
 		'X-RapidAPI-Host': 'nba-player-individual-stats.p.rapidapi.com'
 	  }
   };
+  
   useEffect(() => {
     const abortController = new AbortController();
     const arrayRange = (start, stop, step) =>
@@ -25,14 +25,12 @@ const App = () => {
      try { 
        const response = await Promise.all(
          pages.map( async i => {
-           const res = await axios.get(`https://www.balldontlie.io/api/v1/players?page=${i}&per_page=100`, {signal: abortController.signal})
-          return res;  
-          })
-      )
-       const response2 = await axios.get('https://nba-player-individual-stats.p.rapidapi.com/players', options)
-       setPlayers(response2)
-       const filtered = response.filter(i => i.data.data.some(t => t.first_name === 'LeBron' && t.last_name === "James"));
-       console.log(filtered[0].data.data.filter(i => i.first_name === "LeBron" && i.last_name === "James"));
+           return await axios.get(`https://www.balldontlie.io/api/v1/players?page=${i}&per_page=100`, {signal: abortController.signal})
+          }))
+       const activeResponse = await axios.get('https://nba-player-individual-stats.p.rapidapi.com/players', options)
+       response.map(i => i.data.data.map(t => totalPlayers.push(t)))
+       const playersOnTeam = activeResponse.data.filter(i => i.team !== null).map(i => i.firstName + i.lastName)
+       setActivePlayers(totalPlayers.filter(i => playersOnTeam.includes(i.first_name + i.last_name)));
      } catch (error) {
        if (error.response) {
          console.log(error.response.data);
@@ -40,22 +38,17 @@ const App = () => {
          console.log(error.response.headers); 
        } else if (error.request) {
          console.log(error.request);
-       } else if (error.name === "AbortError") {
-         console.log("aborted");
        } else {
         console.log(error.message);
        }
      }
-     
    }
    getData();
-   
-  // setActivePlayers(totalPlayers);
    return () => abortController.abort();
   },[])
- 
- 
-  return (
+console.log(activePlayers);
+
+ return (
     <div className="App">
      
     </div>

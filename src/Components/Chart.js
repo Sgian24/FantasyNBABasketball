6e51e9f122c208a0c720caafddc70068e13ae421
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2023-05-15 18:25:04 -0400

    Add league average bar.

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -2,53 +2,73 @@ import { ResponsiveBar } from "@nivo/bar";
 
 const Chart = ({activePlayers, roster}) => {
     
-    const averagePoints = parseFloat((activePlayers.map(i => i.avg.pts)
+  const averagePoints = parseFloat((activePlayers.map(i => i.avg.pts)
     .reduce((accumulator, currentValue) => accumulator + currentValue, 0)/441)
     .toFixed(1));
 
-   const testing = roster.map(i => ({
+  const rosterAveragePoints = roster.map(i => ({
         ppg:i.avg.pts,
         Player: i.first_name + " " + i.last_name,
+        color: "#56A6A9"
         }))
-   const testing2 = {
+  const leagueAveragePoints = {
         ppg: averagePoints,
-        Player: "League Average"
+        Player: "League Average",
+        color: "#8356A9"
    }
    
-   const con = testing.concat(testing2)
-   console.log(activePlayers);
-    const Bar = () => {
-        return (
-            <ResponsiveBar 
-                data={con}
-                keys={["ppg"]}
-                indexBy="Player"
-                margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-      padding={0.4}
-      valueScale={{ type: "linear" }}
-      colors="#3182CE"
-      animate={true}
-      enableLabel={false}
-      axisTop={null}
-      axisRight={null}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "Points per game",
-        legendPosition: "middle",
-        legendOffset: -40
-      }}
-      axisBottom={{
-        tickRotation: -50
-      }}
-            />
-        )
+   const totalAveragePoints = rosterAveragePoints.concat(leagueAveragePoints)
+   
+   const getColor = (bar) => bar.data.color
+   
+   const themeTest = {
+    "axis": {
+        "ticks":{
+            "text": {
+                "fontSize": 8
+            }
+        }
     }
-    console.log("object", con);
+   }
+
+   const Bar = () => {
+        return (
+          <>
+            <ResponsiveBar 
+              data={totalAveragePoints}
+              keys={["ppg"]}
+              indexBy="Player"
+              margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+              padding={0.4}
+              valueScale={{ type: "linear" }}
+              colors={getColor}
+              animate={true}
+              enableLabel={false}
+              enableGridX={false}
+              enableGridY={false}
+              axisTop={null}
+              axisRight={null}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "Points per game",
+                legendPosition: "middle",
+                legendOffset: -40
+              }}
+              axisBottom={{
+                tickRotation: -40,
+                tickPadding: 2
+              }}
+              theme={themeTest}
+          />
+        </>   
+      )
+    }
+   
     return (
         <>
-        <div style={{height:500}}>
+        <div style={{height:300}}>
             <Bar/>
         </div>
         </>

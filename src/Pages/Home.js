5a3d230e128a3a3ccca807cfff736eaa6ff9ffc9
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2023-08-23 17:47:45 -0400

    Manually add missing free agent data.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,7 +12,6 @@ import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useEffect, useState, useRef } from 'react';
 import {MemoChart} from '../Components/Chart';
 import Radar from '../Components/Radar';
-import RadarTest from '../Components/RadarTest';
 
 const Home = () => {
 
@@ -22,13 +21,15 @@ const Home = () => {
   const [activePlayers, setActivePlayers] = useState([]);
   const [sort, setSort] = useState("PPG");
   const [playerFilter, setPlayerFilter] = useState("");
-  const [localRoster, setLocalRoster] = useState([]);
   const [roster, setRoster] = useState([]);
   const [playerID, setPlayerID] = useState("");
   const [player, setPlayer] = useState({})
+  const [chart, setChart] = useState("PPG")
 
   const navigate = useNavigate();
-  
+
+  const refTest = useRef()
+
   const options = {
     method: 'GET',
 	  headers: {
@@ -52,29 +53,54 @@ const Home = () => {
        const response = await Promise.all(
          pages.map( async i => {
            return await axios.get(`https://www.balldontlie.io/api/v1/players?page=${i}&per_page=100`, {signal: abortController.signal})
-          }))
-          
-       const activeResponse = await axios.get('https://nba-player-individual-stats.p.rapidapi.com/players', options)
+          }))    
        
+       const activeResponse = await axios.get('https://nba-player-individual-stats.p.rapidapi.com/players', options) 
+      
        response.map(i => i.data.data.map(t => totalPlayers.push(t)))
+
+       /* NBA free agents that switched teams had incomplete data*/
+       const freeAgents = ["Fred", "Dillon", "Donte", "Bruce", "Gabe", "Dennis", "Eric", "Lonnie", "OG"]
+       const freeAgentHeadShots = [{Name:"Fred",
+                            headShot: "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/2991230.png"},
+                           {Name:"Dillon",
+                            headShot: "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3155526.png"}, 
+                           {Name:"Donte",
+                            headShot:"https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3934673.png"}, 
+                           {Name:"Bruce",
+                            headshot:"https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4065670.png"}, 
+                           {Name:"Gabe",
+                            headShot:"https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3137259.png"}, 
+                           {Name:"Dennis",
+                            headShot:"https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3032979.png"},  
+                           {Name:"Eric",
+                            headShot:"https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3431.png"}, 
+                           {Name:"Lonnie",
+                            headShot:"https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4277890.png"}, 
+                           ]
+
        const activePlayersFiltered = totalPlayers.filter(i => activeResponse.data
-        .filter(i => i.team !== null)
+        .filter(i => i.team !== null || freeAgents.includes(i.firstName))
         .map(i => i.firstName + i.lastName)
         .includes(i.first_name + i.last_name) && i.id !== 448)
+        
         // Gary Trent Jr is included a second time with a different id(448) hence the filter 
        activePlayersFiltered.map(i => playerIDs.push(i.id))
        const slicedArray_1 = playerIDs.slice(0, 200)
-       const slicedArray_2 = playerIDs.slice(200, 500)
+       const slicedArray_2 = playerIDs.slice(200, 600)
        const playerIDs_1 = slicedArray_1.map(i => `&player_ids[]=${i}`).join("") 
        const playerIDs_2 = slicedArray_2.map(i => `&player_ids[]=${i}`).join("") 
        const seasonResponse = await axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022${playerIDs_1}`, {signal: abortController.signal})
        const seasonResponse_2 = await axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022${playerIDs_2}`, {signal: abortController.signal})
        const seasonResponseFull = seasonResponse.data.data.concat(seasonResponse_2.data.data)
-       console.log(seasonResponseFull);
+       console.log(seasonResponseFull)
        activePlayersFiltered.forEach(i => {i.avg = seasonResponseFull.filter(t => i.id === t.player_id)[0];
-        i.headShot = activeResponse.data.filter(t => (i.first_name + i.last_name).includes(t.firstName + t.lastName))[0].headShotUrl
-      }) 
+        i.headShot = freeAgents.includes(i.first_name)? freeAgentHeadShots.filter(t => t.Name === i.first_name)[0]?.headShot: 
+        activeResponse.data.filter(t => (i.first_name + i.last_name).includes(t.firstName + t.lastName))[0].headShotUrl
+      })
        setLocal(activePlayersFiltered.filter(i => i.avg !== undefined))
+       console.log("locall", activePlayersFiltered.filter(i => i.first_name === "Bruce"));
+       console.log(freeAgentHeadShots);
      } catch (error) {
        if (error.response) {
          console.log(error.response.data);
@@ -102,7 +128,7 @@ const Home = () => {
       setActivePlayers(localPlayers.filter(i => i.avg.games_played > 9))
    }
   }, [local]);
-
+ 
   useEffect(() => {
     const fetchRoster = async () => {
       if (user.uid) {
@@ -113,17 +139,23 @@ const Home = () => {
     fetchRoster()
   },[user])
 
-  const deleteRoster = async (playerIDD) => {
-    const updatedRoster = roster.filter(i => i.id !== playerIDD)
+  useEffect(() => {
+    refTest.current = playerID
+  })
+
+  const deleteRoster = async (ID) => {
+    const updatedRoster = roster.filter(i => i.id !== ID)
     const rosterRef = doc(firestore, "users", user.uid)
     await updateDoc(rosterRef, {
       roster: updatedRoster
     })
     const docSnap = await getDoc(rosterRef)
     setRoster(docSnap.data().roster)
-    setPlayerID(12345678)
+    if (refTest.current === ID) {
+      setPlayerID(12345)
+    }
   }
-
+  
   const handleChange = (event) => {
     setPlayerFilter(event.target.value)
   }
@@ -145,7 +177,7 @@ const Home = () => {
           </Row>
           <Row>
             <Col md="6">
-              <RosterDashboard roster={roster} playerID={playerID} setPlayerID={setPlayerID} setPlayer={setPlayer} deleteRoster={deleteRoster}/>
+              <RosterDashboard roster={roster} setPlayerID={setPlayerID} deleteRoster={deleteRoster}/>
             </Col>
             <Col md="6"><Radar roster={roster} setPlayer={setPlayer} setPlayerID={setPlayerID} playerID={playerID} player={player} /><MemoChart activePlayers={activePlayers} roster={roster}/></Col>
           </Row>

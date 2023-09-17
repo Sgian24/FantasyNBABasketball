58251fd5a2ea49509c1609abd8b476a8ceb345d0
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2023-09-17 18:23:08 -0400

    Add ebounds, ssists, locks and steals data to bar chart.

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -3,27 +3,33 @@ import { ResponsiveBar } from "@nivo/bar";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 
-const Chart = ({activePlayers, roster}) => {
-
-  const [rebounds, setRebounds] = useState("pts")
+const Chart = ({activePlayers, roster, chartType, setChartType}) => {
     
-  const averagePoints = parseFloat((activePlayers.map(i => i.avg.pts)
+  const averageStats = parseFloat((activePlayers.map(i => i.avg[chartType])
     .reduce((accumulator, currentValue) => accumulator + currentValue, 0)/441)
     .toFixed(1));
 
-  const rosterAveragePoints = roster.map(i => ({
-        ppg:i.avg[rebounds],
+  const rosterAverageStats = roster.map(i => ({
+        [chartType === "pts"? "PPG"
+        : chartType === "reb"? "RPG"
+        : chartType === "ast"? "APG"
+        : chartType === "blk"? "BPG"
+        : "SPG"]: i.avg[chartType],
         Player: i.first_name + " " + i.last_name,
         color: "#56A6A9"
         }))
         
-  const leagueAveragePoints = [{
-        ppg: averagePoints,
+  const leagueAverageStats = [{
+        [chartType === "pts"? "PPG"
+        : chartType === "reb"? "RPG"
+        : chartType === "ast"? "APG"
+        : chartType === "blk"? "BPG"
+        : "SPG"]: averageStats,
         Player: "League Average",
         color: "#8356A9"
    }]
    
-   const totalAveragePoints = leagueAveragePoints.concat(rosterAveragePoints)
+   const totalAverageStats = leagueAverageStats.concat(rosterAverageStats)
    
    const getColor = (bar) => bar.data.color
    
@@ -39,15 +45,26 @@ const Chart = ({activePlayers, roster}) => {
 
   return (
           <div style={{height: 300}}>
-            <div style={{paddingTop: 10}}><Button variant="outline-secondary">Rebounds</Button><Button variant="outline-secondary">Assists</Button><Button variant="outline-secondary">Blocks</Button><Button variant="outline-secondary">Steals</Button></div>
+            <div style={{paddingTop: 10}}>
+              <Button style={{width: "6vw",}}variant="outline-secondary" onClick={() => setChartType("pts")}>Points</Button>
+              <Button variant="outline-secondary" onClick={() => setChartType("reb")}>Rebounds</Button>
+              <Button variant="outline-secondary" onClick={() => setChartType("ast")}>Assists</Button>
+              <Button variant="outline-secondary" onClick={() => setChartType("blk")}>Blocks</Button>
+              <Button variant="outline-secondary" onClick={() => setChartType("stl")}>Steals</Button>
+            </div>
             <ResponsiveBar style={{border: "solid 1px black"}}
-              data={totalAveragePoints}
-              keys={["ppg"]}
+              data={totalAverageStats}
+              keys={[chartType === "pts"? "PPG"
+                    : chartType === "reb"? "RPG"
+                    : chartType === "ast"? "APG"
+                    : chartType === "blk"? "BPG"
+                    : "SPG"]}
               indexBy="Player"
               margin={{ top: 50, right: 20, bottom: 100, left: 60 }}
               padding={0.4}
               valueScale={{ type: "linear" }}
               colors={getColor}
+              layout={"vertical"}
               animate={true}
               enableLabel={false}
               enableGridX={false}
@@ -58,7 +75,11 @@ const Chart = ({activePlayers, roster}) => {
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: "Points per game",
+                legend: `${chartType === "pts"? "Points"
+                        : chartType === "reb"? "Rebounds"
+                        : chartType === "ast"? "Assists"
+                        : chartType === "blk"? "Blocks"
+                        : "Steals" } per game`,
                 legendPosition: "middle",
                 legendOffset: -40
               }}
